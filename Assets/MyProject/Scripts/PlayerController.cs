d6aa37d533392d129c543140a683cc53bd149256
edvaldomelquiades <edmcosta@firjan.com.br>
2025-10-28 11:06:58 -0300

    Add movement system

---
+++ Assets/MyProject/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    // Components
+    private Rigidbody2D rb2d;
+
+    // Variables
+    [Header("Movement System")]
+    [SerializeField] private float moveSpeed = 1f;
+
+    [Header("Fly System")]
+    [SerializeField] private float jumpForce = 100f;
+    [SerializeField] private bool canFly = false;
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        // Move o pássaro para frente
+
+        //---Método 1: Mais legível---
+        //Vector2 vel = rb2d.velocity;
+        //vel.x = moveSpeed;
+        //rb2d.velocity = vel;
+
+        //---Método 2: Mais utilizado---
+        rb2d.velocity = new Vector2 (moveSpeed, rb2d.velocity.y);
+    }
+}
